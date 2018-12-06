a5163bab0555f5e2709ed9475af5b1a3be94d503
SC-201712291012\Administrator <895239330@qq.com>
2018-12-06 15:58:47 +0800

    Initial commit

---
+++ skyegine/src/main/java/com/qjx/skyegine/SkyegineApplication.java
@@ -0,0 +1,12 @@
+package com.qjx.skyegine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyegineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyegineApplication.class, args);
+    }
+}
---
+++ skyegine/src/test/java/com/qjx/skyegine/SkyegineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qjx.skyegine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkyegineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
